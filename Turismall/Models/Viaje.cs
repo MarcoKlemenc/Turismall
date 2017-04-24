6ebb68e53f479fff5933376b01f31557442d8725
MarcoKlemenc <marcoklemenc@gmail.com>
2017-04-23 22:16:09 -0300

    Se agrega modelo para viaje
    Por ahora solamente tiene ID y nombre.

---
+++ Turismall/Models/Viaje.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Turismall.Models
+{
+    public class Viaje
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+    }
+}
