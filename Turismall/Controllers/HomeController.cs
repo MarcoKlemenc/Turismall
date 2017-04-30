b3c2dc9d291cf65d99ea63fc58aaa54bbb714538
rocioechegaraysierra <rocio.echegaray.sierra@gmail.com>
2017-04-30 13:40:49 -0300

    Quiénes somos

--- Turismall/Controllers/HomeController.cs
+++ Turismall/Controllers/HomeController.cs
@@ -15,8 +15,6 @@ namespace Turismall.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
