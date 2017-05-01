51f4c0b0e8dc64fc2a3564a6a41b2ea8e228d5f9
MarcoKlemenc <marcoklemenc@gmail.com>
2017-04-30 22:23:42 -0300

    Se impide crear un viaje sin nombre

--- Turismall/Models/Viaje.cs
+++ Turismall/Models/Viaje.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace Turismall.Models
     public class Viaje
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "El nombre no puede estar vacío.")]
         public string Nombre { get; set; }
         [DisplayName("Descripción")]
         public string Descripcion { get; set; }
