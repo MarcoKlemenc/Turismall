e20d1c0d5c0655e248c0e5e3d8c0c2bca059a2e3
WaldoGaspari <waldogaspari@gmail.com>
2017-05-06 01:07:01 -0300

    Se agrega Nota a la tabla del viaje

---
+++ Turismall/Data/Migrations/20170506033515_[nota].cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Turismall.Data.Migrations
+{
+    public partial class nota : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Viaje",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nota",
+                table: "Viaje",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nota",
+                table: "Viaje");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Viaje",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
