a73728161bf011932f26f0bd0d4617338992343a
MarcoKlemenc <marcoklemenc@gmail.com>
2017-06-04 22:20:48 -0300

    Se agrega modelo de destino

---
+++ Turismall/Models/Destino.cs
@@ -0,0 +1,10 @@
+﻿namespace Turismall.Models
+{
+    public class Destino
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string FotoPortada { get; set; }
+    }
+}
